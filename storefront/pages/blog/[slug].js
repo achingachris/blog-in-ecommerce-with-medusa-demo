e925134cf8f10ca918bbc5d36a2fe3205b7b905c
chrisachinga <achinga.chris@gmail.com>
2022-05-01 19:46:51 +0300

    blog folder

---
+++ storefront/pages/blog/[slug].js
@@ -0,0 +1,5 @@
+const [slug] = () => {
+  return <div>blog details</div>
+}
+
+export default [slug]
---
+++ storefront/pages/blog/index.js
@@ -0,0 +1,5 @@
+const index = () => {
+  return <div>blog home page</div>
+}
+
+export default index
